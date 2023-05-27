e697484c7db38aa2f24b2bacafe5c0f1264f1aa9
Rajab <rajabdearly@gmail.com>
2023-05-27 15:50:16 +0300

    fixed lua language server in lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,10 +2,10 @@ local lsp = require('lsp-zero').preset({})
 
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'rust_analyzer',
-  'lua-language-server'
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
